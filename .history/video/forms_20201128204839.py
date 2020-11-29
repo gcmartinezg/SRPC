aa0d76ab06394a8933c14c5ee81b48d4ced18181
German Martinez <germancmartinez@hotmail.com>
2020-11-28 22:15:09 -0500

    removing .history

--- .history/video/forms_20201128204839.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class upload(forms.Form):
-    name = forms.CharField(label="Enter name", max_lenght=50)
-    email = forms.EmailField()
-    file = forms.FileField()
--- .history/video/forms_20201128212831.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class upload(forms.Form):
-    name = forms.CharField(label="Enter name", max_length=50)
-    email = forms.EmailField()
-    file = forms.FileField()
