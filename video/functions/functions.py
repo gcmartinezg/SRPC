2a6bfddd9688f1ab182e43ca00b17eaef31e14c1
German Martinez <germancmartinez@hotmail.com>
2020-11-30 00:43:47 -0500

    Added random frame picking. Added plate recognizer service. PENDING: implement clean_up method

--- video/functions/functions.py
+++ video/functions/functions.py
@@ -1,7 +1,7 @@
-
 path = 'video/static/uploads/'
 path_to_uploaded_file = path + 'fileupload'
 path_to_frames = path + 'frames/'
+percentage_to_pick = 0.05
 
 #TODO document functions 
 def handle_uploaded_file(f):
@@ -25,6 +25,7 @@ def save_uploaded_file(f):
 # Opens the Video file
 def extract_frames(f):
     import cv2
+
     cap=cv2.VideoCapture(path_to_uploaded_file + f.name)
     i=0
     while(cap.isOpened()):
@@ -47,6 +48,7 @@ def get_filename(f):
 def validate_video(f):
     import moviepy.editor
     from pathlib import Path
+
     path = path_to_uploaded_file + f.name
     video = moviepy.editor.VideoFileClip(path)
     video_duration_sec = int(video.duration)
@@ -65,11 +67,13 @@ def validate_video(f):
 
 def remove_file(f):
     import os
+
     os.remove(path_to_uploaded_file+f.name)
 
 def validate_file_extension(value):
     import os
     from django.core.exceptions import ValidationError
+
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.mp4', '.mkv', '.avi']
     if not ext.lower() in valid_extensions:
@@ -78,6 +82,7 @@ def validate_file_extension(value):
 def edsr_model(image):
     import cv2
     from cv2 import dnn_superres
+
     sr = dnn_superres.DnnSuperResImpl_create()
     path = 'video/static/models/EDSR_x3.pb'# ... /path/to/EDSR_x3.pb
     sr.readModel(path)
@@ -85,4 +90,35 @@ def edsr_model(image):
     result = sr.upsample(image)
     cv2.imwrite('path', result)# falta guardar en una ruta diferente a la de los frames 
     
+def clean_up():
+    """
+    TODO handle clean up after finishing the processing:
+    Removing the content of the frames folder
+    Removing the root's contents of the uploads folder
+    """
 
+def pick_frames(name):
+    import os
+    from pathlib import Path
+    from math import ceil
+
+    frame_number = len(os.listdir(path_to_frames))
+    frames_to_be_picked = ceil(frame_number * percentage_to_pick)
+
+    random_list = random.sample(range(0, frame_number), frames_to_be_picked)
+    picked_frames = [path_to_frames + name + random_number + '.jpg' for random_number in random_list]
+    
+    return picked_frames
+
+def get_plate(image_path):
+    import requests
+    from pprint import pprint
+
+    regions = ['co'] # Change to your country
+    with open(image_path, 'rb') as fp:
+        response = requests.post(
+            'https://api.platerecognizer.com/v1/plate-reader/',
+            data=dict(regions=regions),  # Optional
+            files=dict(upload=fp),
+            headers={'Authorization': 'Token c237819a800f880ca82b75543315a65330334d3d'})
+    return response.json()
